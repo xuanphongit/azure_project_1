bcc896d20d6dbba90c49a601f928c46fe331f350
Mai Xuân Phong <xuanphongit1109@gmail.com>
2023-07-07 21:31:47 +0700

    add log

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,12 +1,10 @@
-"""
-The flask application package.
-"""
 import logging
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from logging.handlers import StreamHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,5 +13,23 @@ Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+
+stream_handler = StreamHandler()
+stream_handler.setLevel(logging.INFO)
+app.logger.addHandler(stream_handler)
+
+@app.before_first_request
+def setup_logging():
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers.extend(gunicorn_logger.handlers)
+    app.logger.setLevel(gunicorn_logger.level)
+    app.logger.addHandler(stream_handler)
+
+    gunicorn_logger.info('Application initialized')
+
+    @app.after_request
+    def after_request(response):
+        app.logger.info('{} {} {}'.format(request.method, request.path, response.status_code))
+        return response
 
 import FlaskWebProject.views
